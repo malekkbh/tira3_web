4948da769928b7dd9991233c8ecff53b4e84c5fc
Malek Kabaha <malek.kbh@gmail.com>
2025-08-03 15:05:56 +0300

    ..

---
+++ src/app/page1/page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './page1.module.css'
+import './x.css'
+
+const Page = () => {
+    return (
+        <div className={styles.page}>
+            <text className={styles.txt}>test</text>
+            <text className='test'>test</text>
+            
+        </div>
+    );
+}
+
+export default Page;
